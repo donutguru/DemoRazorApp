c2b68d58f876bb191920b0ae872b8e7a3e6b794c
Chris Richardson <chris.richardson@emerson.com>
2022-04-20 11:15:42 -0500

    Make a test fail

--- DemoRazorApp.Test/UnitTest1.cs
+++ DemoRazorApp.Test/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace DemoRazorApp.Test
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(true, true);
+            Assert.AreEqual(true, false);
         }
     }
 }
