c8c25d2e3f07d24fd251d27f4105f89276603e00
Chris Richardson <chris.richardson@emerson.com>
2022-04-21 09:45:03 -0500

    Try a Failing E2E Test

--- E2E/cypress/integration/demo-test.js
+++ E2E/cypress/integration/demo-test.js
@@ -3,7 +3,7 @@
 describe('demonstration cypress test with demo razor app', () => {
     it('should link to something', () => {
        cy.visit('/');
-       cy.contains('a', 'Home').should('exist');
+       cy.contains('a', 'Home').should('not.exist');
        cy.contains('a', 'Privacy').click();
        cy.contains('Privacy Policy').should('exist');
     });
